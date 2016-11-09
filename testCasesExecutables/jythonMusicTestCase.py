23be2ca3bc165586c633eab1ce011ca3cc5d6cc1
ehofesmann <ehofesmann@gmail.com>
2016-11-08 19:57:52 -0500

    Add almost equals precision and test cases 10, 15, and 16

--- testCasesExecutables/jythonMusicTestCase.py
+++ testCasesExecutables/jythonMusicTestCase.py
@@ -128,7 +128,7 @@ class JythonMusicTestCase(unittest.TestCase):
         'testDescription': self.description,
         'testModule': self.testModule,
         'testFunction': self.testFunction,
-        'testName': self.testname,
+        'testName': self.testname[0],
         'inputs': self.inputs,
         'expected': self.outputValue,
         'actual': self.actualResults
